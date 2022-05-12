6c764d02b4f78e890db24b265f8c7e3a5b047262
Alan Tabbernor <alan.tabbernor@translink.ca>
2022-05-12 19:58:34 +0000

    baseMapChangeZoomFix

--- editMap.js
+++ editMap.js
@@ -1722,7 +1722,7 @@ class EditMap {
     var mapBaseMap = L.tileLayer(
       "https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png",
       {
-        maxZoom: 20,
+        maxZoom: 18,
         attribution:
           '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
       }
